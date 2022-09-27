b6b3ac4137b5132fef6c13d7d4058a604a04a423
Juan Solis <juansolis@MacBook-Air-de-Juan.local>
2022-09-27 15:49:49 -0600

    Primer Commit con repositorio inicial y rutas configuradas

---
+++ src/components/NoPage/NoPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./NoPage.css";
+
+const NoPage = () => {
+    return (
+        <div class="noPageWrapper">
+            404
+        </div>
+    )
+}
+
+export default NoPage;
