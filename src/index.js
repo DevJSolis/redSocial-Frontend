afc401260e218e0ab40faf3624a1c8aa5ef2c6a9
KeilaCL <kncortezl@correo.url.edu.gt>
2022-11-01 18:45:20 -0600

    Recovery Password inicios

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import NoPage from './components/NoPage/NoPage';
 import Home from './components/Home/Home';
 import Login from './Pages/Login';
 import Register from './Pages/Register/Register';
-
+import RecoveryPassword from './Pages/Recovery-password/RecoveryPassword';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,6 +19,7 @@ root.render(
         <Route path="/" element={<Home />}/>
         <Route path="/login" element={<Login />}/>
         <Route path="/register" element={<Register />}/>
+        <Route path="/recovery-password" element={<RecoveryPassword />}/>
         <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
