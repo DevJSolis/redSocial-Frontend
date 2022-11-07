ae0132a4ea25b59ab94a7b0223ffa60fe157e3c8
KeilaCL <kncortezl@correo.url.edu.gt>
2022-11-06 19:52:24 -0600

    Cambios en recovery password

--- src/components/RecoveryPasswordForm/RecoveryPasswordForm.jsx
+++ src/components/RecoveryPasswordForm/RecoveryPasswordForm.jsx
@@ -1,10 +1,32 @@
 import React, { useState } from 'react';
 import './RecoveryPasswordForm.css';
 
-function RecoveryPasswordForm() {
-    return ( 
-        <div className='recoveryPasswordForm'>
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
 
+function RecoveryPasswordForm(props) {
+    return ( 
+
+        <div className='recoveryPasswordForm'>
+            <div className='centeritems'>
+            <h5 className={`RecoveryTitle darkmode_${props.darkMode}`}>
+                            <strong>
+                                Recupera tu contraseña
+                            </strong>              
+            </h5>
+            
+            <div>
+                Ingresa tu correo electronico para buscar tu cuenta.
+            </div>
+            <br/>
+            <div >
+                <input type= "text"  className='formControl' placeholder='Usuario' required ></input>
+            </div>
+            <div>
+            <br/>
+                <Button type="submit" className='formControl_btn'>Buscar</Button>
+            </div>
+            </div>
         </div>
      );
 }
