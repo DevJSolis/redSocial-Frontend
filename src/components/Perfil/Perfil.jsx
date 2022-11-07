f23b95f1fbb7bd98656da47b1625df806cd00e9c
Mansi1108 <mansillamelissa@gmail.com>
2022-11-06 19:55:50 -0600

    Layout predefinido

---
+++ src/components/Perfil/Perfil.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Perfil.css";
+const Perfil = () => {
+  return (
+    <div className='containerP'>
+        <div className='header'>
+             <div className=''></div>
+        </div>
+        <div className='namePerson'>
+            Nombre
+        </div>
+        <div className='fotoP'></div>
+        <div className='amigos'></div>
+        <div className='fotos'></div>
+        <div className='postP'></div>
+
+    </div>
+
+
+
+  )
+}
+
+export default Perfil
