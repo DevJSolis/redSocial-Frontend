c2970bfbaa2612587f6c3a603226150f5276c20b
Mansi1108 <mansillamelissa@gmail.com>
2022-11-06 19:55:50 -0600

    Layout predefinido

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import App from './App';
 import NoPage from './components/NoPage/NoPage';
 import Home from './components/Home/Home';
 import Post from './components/Post/Post';
+import Perfil from './components/Perfil/Perfil';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,6 +16,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/perfil" element={<Perfil />}/>  
         <Route path="/" element={<Post />}/>
         <Route path="*" element={<NoPage />} />
       </Routes>
