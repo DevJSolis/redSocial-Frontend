78ca487679fc2ab35a7aa3d8d277035564da6f14
Bushinii <jorgeg.baldizon@gmail.com>
2022-11-09 10:44:44 -0600

    ListOfPosts Fixed
    Se realizaron correcciones a la lista de Post. Eliminando funciones inecesarias

--- src/components/Feed/ListOfPosts.jsx
+++ src/components/Feed/ListOfPosts.jsx
@@ -1,17 +1,10 @@
-function Post(props) {
-  return <li>{ props.brand }</li>;
-}
-
 function ListOfPosts() {
   const posts =[]; // Get ListOfPosts(API)
   return (
     <>
       <ul>
-        {posts.map((post) => <Post brand={post} />)}
+        {posts.map((post) => <Post post/>)}
       </ul>
     </>
   );
 }
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ListOfPosts/>);
