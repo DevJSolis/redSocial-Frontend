bba7aeae0d0766862d730a4a55fb37ba7bf7df8d
Andrea-Gudiel-Zepeda <[andreamaria1d@gmail.com]>
2022-11-09 01:35:11 -0600

    avance

---
+++ src/Pages/New-Password/NewPassword.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import backgroundNewPasswordDesktop from '../../assets/newPassword/newPassBackground.png';
+import backgroundNewPasswordDesktopDark from '../../assets/newPassword/newPassBackgroundDark.png';
+import './NewPassword.css';
+import NewPasswordForm from '../../components/NewPasswordForm/NewPasswordForm';
+
+function NewPassword(){
+    const [darkModeSwitchState, setdarkModeSwitchState] = useState(false);
+    const handleChange=(e)=>{
+        setdarkModeSwitchState(!darkModeSwitchState)   
+    }
+
+    return(
+        <Container fluid className='newPassword'  style={{ backgroundImage: `url(${ darkModeSwitchState ? backgroundNewPasswordDesktopDark : backgroundNewPasswordDesktop })`}}>
+         <Form.Check
+                        className='darkModeSwitch'
+                        type="switch"
+                        id="custom-switch"
+                        label="Dark Mode"
+                        defaultChecked={darkModeSwitchState}
+                        onChange={handleChange}
+                    />
+           <Row className='newPasswordFormWrapper'>
+            <Col xs={12} md={5}>
+            <NewPasswordForm/>  
+            </Col>
+           </Row>
+        </Container>
+    );
+}
+
+export default NewPassword;
---
+++ src/components/NewPasswordForm/NewPasswordForm.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './NewPasswordForm.css';
+
+function NewPasswordForm() {
+    return ( 
+        <div className='newPasswordForm'>
+
+        </div>
+     );
+}
+
+export default NewPasswordForm;
