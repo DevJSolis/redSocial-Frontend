f3424990a65c6892c1a39880a94250ce96f3f54a
Aylinne Recinos <aylinneart@gmail.com>
2022-11-22 13:52:26 -0600

    Agrega cambios al post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,49 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Post.css";
 import Comment from "../../assets/img/comment.png";
-import Share from "../../assets/img/share.png";
 import Heart from "../../assets/img/like.png";
 import NotLike from "../../assets/img/notlike.png";
 import { likePost } from "../../api/PostsRequests";
 import { useSelector } from "react-redux";
+import { formatDistance } from "date-fns";
+import { getUser } from "../../api/UserRequests";
 
 const Post = ({ data }) => {
   const { user } = useSelector((state) => state.authReducer.authData);
+  const [postUser, setpostUser] = useState({});
   const [liked, setLiked] = useState(data.likes.includes(user._id));
-  const [likes, setLikes] = useState(data.likes.length)
+  const [likes, setLikes] = useState(data.likes.length);
 
-  
+  useEffect(() => {
+    const fetchPerson = () => {
+      getUser(data.userId).then((response) => {
+        console.log(response.data);
+        setpostUser(response.data);
+      });
+    };
+    fetchPerson();
+  }, [data]);
+
+  //const dateToFormat = format(new Date(data.createdAt), 'dd/mm/yyyy');
+  const dateToFormat = formatDistance(new Date(data.createdAt), new Date(), {
+    addSuffix: true,
+  });
+
   const handleLike = () => {
     likePost(data._id, user._id);
     setLiked((prev) => !prev);
-    liked? setLikes((prev)=>prev-1): setLikes((prev)=>prev+1)
+    liked ? setLikes((prev) => prev - 1) : setLikes((prev) => prev + 1);
   };
   return (
     <div className="Post">
-      <img
-        src={data.image ? process.env.REACT_APP_PUBLIC_FOLDER + data.image : ""}
-        alt=""
-      />
-
-      <div className="postReact">
-        <img
-          src={liked ? Heart : NotLike}
-          alt=""
-          style={{ cursor: "pointer" }}
-          onClick={handleLike}
-        />
-        <img src={Comment} alt="" />
-        <img src={Share} alt="" />
-      </div>
-
-      <span style={{ color: "var(--gray)", fontSize: "12px" }}>
-        {likes} likes
-      </span>
       <div className="detail">
-        <span>
-          <b>{data.name} </b>
+        <div style={{ marginLeft: "2%" }}>
+          <b>{postUser.username}</b>
+          <br />
+          <span style={{ color: "var(--gray)", fontSize: "12px" }}>
+            {dateToFormat}
+          </span>
+        </div>
+        <div style={{ margin: "2%" }}>
+          <span>{data.desc}</span>
+        </div>
+        <img
+          src={
+            data.image ? process.env.REACT_APP_PUBLIC_FOLDER + data.image : ""
+          }
+          alt=""
+        />
+        <div className="postReact" style={{ marginLeft: "2%" }}>
+          <img
+            src={liked ? Heart : NotLike}
+            alt=""
+            style={{ cursor: "pointer" }}
+            onClick={handleLike}
+          />
+        </div>
+
+        <span
+          style={{ color: "var(--gray)", fontSize: "12px", marginLeft: "2%" }}
+        >
+          {likes} likes
         </span>
-        <span>{data.desc}</span>
       </div>
     </div>
   );
