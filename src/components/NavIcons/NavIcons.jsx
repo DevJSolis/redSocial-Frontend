49c534ee15208d06435f678b9765f2c5dfd1f79c
Javier Andrés <54732923+jamg1906@users.noreply.github.com>
2022-11-22 16:53:11 -0600

    Navbar updated

--- src/components/NavIcons/NavIcons.jsx
+++ src/components/NavIcons/NavIcons.jsx
@@ -3,10 +3,19 @@ import React from "react";
 import Home from "../../assets/img/home.png";
 import Noti from "../../assets/img/noti.png";
 import Comment from "../../assets/img/comment.png";
+import Logout from "../../assets/img/Logout.png"
+import ProfileIcon from "../../assets/img/profilemenu.png"
 import { UilSetting } from "@iconscout/react-unicons";
 import { Link } from "react-router-dom";
+import { logout } from "../../actions/AuthActions";
+import { useDispatch, useSelector } from "react-redux";
 
 const NavIcons = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.authReducer.authData);
+  const handleLogOut = ()=> {
+    dispatch(logout())
+  }
   return (
     <div className="navIconsCard">
       <div className="navIcons">
@@ -15,17 +24,17 @@ const NavIcons = () => {
         </Link>
       </div>
       <div className="navIcons">
-        <Link to={`/profile`}>
-          <UilSetting />
+      <Link to={`/profile/${user._id}`}>
+           <img src={ProfileIcon} alt="" />
         </Link>
-      </div>
-      <div className="navIcons">
-        <img src={Noti} alt="" />
-      </div>
+        </div>
       <div className="navIcons">
         <Link to="../chat">
           <img src={Comment} alt="" />
         </Link>
+      </div>
+      <div className="navIcons">
+         <img src={Logout} alt=""onClick={handleLogOut} />
       </div>
     </div>
   );
